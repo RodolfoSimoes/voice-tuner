64aa541d9200d510049f1c9d1f0982361accde02
Rodolfo <rodolfo@quicktech.no>
2016-09-06 17:39:30 -0300

    configure gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,60 @@
-var gulp = require('gulp'),
-    gutil = require('gulp-util'),
-    sass = require('gulp-sass'),
-    concat = require('gulp-concat'),
-    rename = require('gulp-rename'),
+var gulp        = require('gulp'),
+    gutil       = require('gulp-util'),
+    sass        = require('gulp-sass'),
+    concat      = require('gulp-concat'),
+    rename      = require('gulp-rename'),
     cssPrefixer	= require('gulp-autoprefixer'),
-    cssMinify = require('gulp-minify-css'),
-    uglify = require('gulp-uglify'),
-    imageMin = require('gulp-imagemin');
+    cssMinify   = require('gulp-minify-css'),
+    uglify      = require('gulp-uglify'),
+    imageMin    = require('gulp-imagemin'),
+    babel       = require('gulp-babel'),
+    htmlmin     = require('gulp-htmlmin');
 
 var PATH_SRC = './src/';
 var PATH_DIST = './dist/';
+var PATH_NODE = './node_modules/';
 
 /* scripts */
 
-gulp.task('scripts', function() {});
+var scriptsSrc = [
+    // libs
+    PATH_NODE + 'vue/vue.js'
+    //,
+    // PATH_SRC + 'scripts/config.js',
+    // // others
+    // // elements
+    // PATH_SRC + 'scripts/main.js'
+];
 
-gulp.task('scripts-min', function() {});
+var scriptsDist = PATH_DIST + 'scripts/';
+
+gulp.task('scripts', function() {
+    return gulp
+        .src(scriptsSrc)
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(concat('app.js'))
+        .pipe(gulp.dest(scriptsDist));
+});
+
+gulp.task('scripts-min', function() {
+    return gulp
+        .src(scriptsSrc)
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(concat('app.js'))
+        .pipe(uglify())
+        .pipe(rename('app.js'))
+        .pipe(gulp.dest(scriptsDist));
+});
 
 /* styles */
 
 var stylesSrc = [
-    PATH_SRC + 'styles/main.scss'
+    PATH_SRC + 'styles/main.scss',
+    PATH_SRC + 'elements/**/*.scss'
 ];
 
 var stylesDist = PATH_DIST + 'styles/';
@@ -73,11 +107,52 @@ gulp.task('images-min', function() {
         .pipe(gulp.dest(imagesDist));
 });
 
-// gulp.task('views', function() {});
+/* views */
 
-gulp.task('default', ['images', 'styles']);
+var viewsSrc = [
+    PATH_SRC + 'views/*.html'
+];
 
-gulp.task('dist', ['images-min', 'styles-min']);
+var viewsDist = PATH_DIST;
 
-//
-// gulp.watch('', []);
+gulp.task('views', function() {
+    return gulp
+        .src(viewsSrc)
+        .pipe(gulp.dest(viewsDist));
+});
+
+gulp.task('views-min', function() {
+    return gulp
+        .src(viewsSrc)
+        .pipe(htmlmin({collapseWhitespace: true}))
+        .pipe(gulp.dest(viewsDist));
+});
+
+/* views */
+
+var templatesSrc = [
+    PATH_SRC + 'elements/**/*.html'
+];
+
+var templatesDist = PATH_DIST + 'templates/';
+
+gulp.task('templates', function() {
+    return gulp
+        .src(templatesSrc)
+        .pipe(concat('templates.js'))
+        .pipe(gulp.dest(templatesDist));
+});
+
+gulp.task('templates-min', function() {
+    return gulp
+        .src(templatesSrc)
+        .pipe(htmlmin({collapseWhitespace: true}))
+        .pipe(concat('templates.js'))
+        .pipe(gulp.dest(templatesDist));
+});
+
+gulp.task('default', ['images', 'scripts', 'styles', 'views', 'templates']);
+
+// gulp.task('dist', ['images-min', 'scripts-min', 'styles-min', 'views-min', 'templates-min']);
+
+// gulp.watch('', ['default']);
